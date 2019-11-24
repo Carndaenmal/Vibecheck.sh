462529a05ff11396ff50b372a0d85fda0b046a0f
Dane MALENFANT <dmalen3@teaching.CS.McGill.CA>
2019-11-24 00:03:16 -0500

    VIBECHECK

---
+++ vibecheck.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+F_VDOUBLE="\033#6"
+str=" ##VIBECHECK## "
+vibe=$(date -d "${date}" +"%s")
+i=0 
+printf "CHECKING VIBE:  \n"
+sleep 2.5;
+while true;do echo -ne "$str\r";i=$(($i+1)); str="$str ."; 
+	if (($(($i % 10)) == 0 )) 
+	then 
+		str="$str $i%"
+		#echo -ne "$i%%"
+		
+	else
+		str="$str ."
+	fi
+	if (($i == 95))
+       	then
+		break;
+	fi
+	sleep .1;clear;done &
+sleep 10; 
+printf "100%%\n"
+printf "\n"
+echo -ne `date`
+printf "\n"
+if (( $vibe % 3 == 0 )) #more accurate than astrology
+then
+	printf "\n${F_VDOUBLE}~ ~ VIBING ~ ~\n\n ᕙ(* •̀ ᗜ •́ *)ᕗ\n\n"
+else
+	printf "\n${F_VDOUBLE}~ ~ NOT VIBINING ~ ~\n\n ᕦ༼ •́ ‸ •̀ ༽ᕤ\n\n"
+fi
